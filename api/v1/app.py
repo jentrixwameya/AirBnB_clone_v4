38d7a071006cbd43edc9da1d9ebc30835535a1f7
richardoluwadamilola <oluwatobyrichard@gmail.com>
2023-02-22 23:27:36 -0800

    API status

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from flasgger.utils import swag_from
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     host = environ.get('HBNB_API_HOST')
     port = environ.get('HBNB_API_PORT')
     if not host:
-        host = '0.0.0.0'
+        host = '*'
     if not port:
         port = '5000'
     app.run(host=host, port=port, threaded=True)
